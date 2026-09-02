edb872a309bd14ce0de0c0d1ecea86b6a92d9043
William de Paula <gomes.william18@yahoo.com.br>
2026-09-02 20:29:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -48,12 +48,18 @@ col1, col2 = st.columns(2)
 with col1:
 	st.subheader("Distribuição de estudantes por depressão")
 	fig, ax = plt.subplots(figsize=(8, 6))
+
+	fig.patch.set_alpha(0)
+	ax.patch.set_alpha(0)
+	ax.tick_params(axis="both", colors="white")
+
 	total = quantidade.sum()
 
 	ax.pie(
 		quantidade,
 		labels=quantidade.index,
-		autopct=lambda pct: f"{pct:.1f}%\n({int(pct * total / 100):,})"
+		autopct=lambda pct: f"{pct:.1f}%\n({int(pct * total / 100):,})",
+		textprops={"color": "white"}
 	)
 
 	st.pyplot(fig)
@@ -64,12 +70,25 @@ media.index = ["Sem depressão", "Com depressão"]
 with col2:
 	st.subheader("Social Media x Depression")
 	fig, ax = plt.subplots(figsize=(8, 6))
-	media.plot.bar(ax=ax, color=["#1f77b4", "#ff7f0e"])
+
+	fig.patch.set_alpha(0)
+	ax.patch.set_alpha(0)
+
+	media.plot.bar(
+		ax=ax,
+		color=["#1f77b4", "#ff7f0e"]
+	)
+
+	ax.tick_params(axis="both", colors="white")
+	ax.set_ylabel("Horas", color="white")
+	ax.set_title("Média de uso de redes sociais", color="white")
+
 	ax.bar_label(
         ax.containers[0],
         labels=[f"{valor:.2f} h" for valor in media],
         padding=3,
-		fontsize=14
+		fontsize=14,
+		color="white"
     )
 
 	ax.set_title("Média de uso de redes sociais")
