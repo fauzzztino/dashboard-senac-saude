cfcec01c5ed84515bc07d8ba2ecf8b0774531881
William de Paula <gomes.william18@yahoo.com.br>
2026-09-02 19:33:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,51 @@ ax.set_xticklabels(rotulos_tempo)
 
 # Mostra no Streamlit
 st.pyplot(fig)
+
+sem_depressão = dados[dados["Depression"] == False]
+com_depressão = dados[dados["Depression"] == True]
+quantidade = dados["Depression"].value_counts()
+quantidade.index = ["Sem depressão", "Com depressão"]
+
+col1, col2 = st.columns(2)
+
+with col1:
+	st.subheader("Distribuição de estudantes por depressão")
+	fig, ax = plt.subplots(figsize=(8, 6))
+	total = quantidade.sum()
+
+	ax.pie(
+		quantidade,
+		labels=quantidade.index,
+		autopct=lambda pct: f"{pct:.1f}%\n({int(pct * total / 100):,})"
+	)
+
+	st.pyplot(fig)
+
+media = dados.groupby("Depression")["Social_Media_Hours"].mean()
+media.index = ["Sem depressão", "Com depressão"]
+
+with col2:
+	st.subheader("Social Media x Depression")
+	fig, ax = plt.subplots(figsize=(8, 6))
+	media.plot.bar(ax=ax, color=["#1f77b4", "#ff7f0e"])
+	ax.bar_label(
+        ax.containers[0],
+        labels=[f"{valor:.2f} h" for valor in media],
+        padding=3,
+		fontsize=14
+    )
+
+	ax.set_title("Média de uso de redes sociais")
+	ax.set_ylabel("Horas")
+	ax.tick_params(axis="x", labelrotation=0)
+	ax.set_ylim(0, 4)
+
+	ax.plot(
+		[0, 1],
+		[media.iloc[0], media.iloc[0]],
+		linestyle="--",
+		color="#1f77b4"
+	)
+
+	st.pyplot(fig)
