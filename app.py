f96b91109f8b429d0429d16a3d63a6d62684f6a2
fauzzztino <gabriel.kaneki2003@gmail.com>
2026-09-02 19:47:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,80 +4,28 @@ import matplotlib.pyplot as plt
 
 st.title("Rede Social x Estresse")
 
-# Carrega os dados
 df = pd.read_csv("base_tratada.csv")
-
-# Pega 10 registros únicos e ordena pelo tempo de tela
 df_grafico = df.drop_duplicates(subset=["Social_Media_Hours"]).sample(10, random_state=42)
 df_grafico = df_grafico.sort_values(by="Social_Media_Hours")
 
-# Função simples para formatar as horas (ex: 2.5 vira 2:30)
 def formata_hora(valor):
     h = int(valor)
     m = int((valor - h) * 60)
     return f"{h}:{m:02d}"
 
-# Cria a lista de horários para o eixo X usando um laço simples
 rotulos_tempo = [formata_hora(t) for t in df_grafico["Social_Media_Hours"]]
 
-# Monta o gráfico
 fig, ax = plt.subplots(figsize=(10, 4))
+
+fig.patch.set_alpha(0)
+ax.patch.set_alpha(0)
+
 ax.bar(range(10), df_grafico["nivel_estresse"], color="#1f77b4")
 
-# Configurações do gráfico
 ax.set_title("Rede Social x Estresse")
 ax.set_xlabel("Tempo de uso de Redes Sociais")
 ax.set_ylabel("Nível de Estresse")
 ax.set_xticks(range(10))
 ax.set_xticklabels(rotulos_tempo)
 
-# Mostra no Streamlit
 st.pyplot(fig)
-
-sem_depressão = dados[dados["Depression"] == False]
-com_depressão = dados[dados["Depression"] == True]
-quantidade = dados["Depression"].value_counts()
-quantidade.index = ["Sem depressão", "Com depressão"]
-
-col1, col2 = st.columns(2)
-
-with col1:
-	st.subheader("Distribuição de estudantes por depressão")
-	fig, ax = plt.subplots(figsize=(8, 6))
-	total = quantidade.sum()
-
-	ax.pie(
-		quantidade,
-		labels=quantidade.index,
-		autopct=lambda pct: f"{pct:.1f}%\n({int(pct * total / 100):,})"
-	)
-
-	st.pyplot(fig)
-
-media = dados.groupby("Depression")["Social_Media_Hours"].mean()
-media.index = ["Sem depressão", "Com depressão"]
-
-with col2:
-	st.subheader("Social Media x Depression")
-	fig, ax = plt.subplots(figsize=(8, 6))
-	media.plot.bar(ax=ax, color=["#1f77b4", "#ff7f0e"])
-	ax.bar_label(
-        ax.containers[0],
-        labels=[f"{valor:.2f} h" for valor in media],
-        padding=3,
-		fontsize=14
-    )
-
-	ax.set_title("Média de uso de redes sociais")
-	ax.set_ylabel("Horas")
-	ax.tick_params(axis="x", labelrotation=0)
-	ax.set_ylim(0, 4)
-
-	ax.plot(
-		[0, 1],
-		[media.iloc[0], media.iloc[0]],
-		linestyle="--",
-		color="#1f77b4"
-	)
-
-	st.pyplot(fig)
